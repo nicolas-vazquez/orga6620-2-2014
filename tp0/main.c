ae8fe32f246ca19b75e36a8f9386fa006c098859
aschicht <nerhomarek@gmail.com>
2014-09-23 15:06:54 -0300

    main modificado

--- tp0/main.c
+++ tp0/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 
 #define false 0
 #define true 1
 #define BUFF_SIZE 1024
 
-struct options {
+struct Options {
     char* separator;
     int starting;
     int increment;
     int join;
     int blank;
 };
+
+void write_line(FILE* fd, struct Options* options, char* c, int header){
+	if (header){        
+		printf("%d%s%c", options->starting, options->separator, *c);
+	}else{
+		printf("%c", *c);
+	}
+	fflush(stdout);
+
+}
+
 
 void show_help() {
     printf("USAGE:\n");
@@ -28,101 +40,148 @@ void show_help() {
     printf("-t, --non-blank\n");
 }
 
-void nl_v1(FILE* fd, struct options* options) {
-    char c;
-    int i = 0;
+void skip_blanks(FILE* fd, struct Options* options, int* i, char* c){
 
-    while ((c = fgetc(fd)) != EOF) {
-        if (c == '\n' && options->blank) {
-            while(c == '\n') {
-                printf("%c", c);
-                c = fgetc(fd);
-            }
-        } else if (c == '\n' && options->join > 0) {
-            while (++i < options->join) {
-                if ((c = fgetc(fd)) != '\n')
-                    break;
-                else
-                    printf("%c", c);
-            }
+    if (*c == '\n' && options->blank) {
+        while(*c == '\n') {
+            write_line(fd, options, c, 0);
+	    //printf("%c", *c);
+            *c = fgetc(fd);
         }
-
-        printf("%d%s%c", options->starting, options->separator, c);
-
-        if (c != '\n') {
-            while ((c = fgetc(fd)) != '\n')
-                printf("%c", c);
-            printf("\n");
+    } else if (*c == '\n' && options->join > 0) {
+        while (++(*i) < options->join) {
+            if ((*c = fgetc(fd)) != '\n')
+                break;
+            else
+	        write_line(fd, options, c, 0);                
+		//printf("%c", *c);
         }
+    }
+
+}
+
+void increment(struct Options* options){
 
         if (!options->increment)
             options->starting += 1;
         else
             options->starting += options->increment;
+}
+
+void parse_line(FILE* fd, struct Options* options, char* c){
+	        
+	write_line(fd, options, c, 1);
+	if (*c != '\n') {
+            while ((*c = fgetc(fd)) != '\n')
+		write_line(fd, options, c, 0);                
+		//printf("%c", *c);
+            write_line(fd, options, "\n", 0);
+	    //printf("\n");
+        }
+
+}
+
+
+void nl_v1(FILE* fd, struct Options* options) {
+    char c;
+    int i = 0;
+
+    while ((c = fgetc(fd)) != EOF) {
+	skip_blanks(fd, options, &i, &c);
+	parse_line(fd, options, &c);
+	increment(options);
     }
 }
 
-void nl_v2(FILE* fd, struct options* options) {
+void write_line_v2(char* lbuf, struct Options* options, char* c, int header){
+	if (header == 1){        
+	        printf("%d%s%s\n", options->starting, options->separator, lbuf);
+	}else{
+		printf("%c", *c);
+	}
+
+}
+
+void skip_blanks_v2(struct Options* options, int* i, char* c, char* fbuf, long* a){
+
+    if (*c == '\n' && options->blank) {
+        while(*c == '\n') {
+            write_line_v2(NULL, options, c, 0);
+	    //printf("%c", *c);
+            *c = fbuf[(*a)++];
+        }
+    } else if (*c == '\n' && options->join > 0) {
+        while (++(*i) < options->join) {
+            if ((*c = fbuf[(*a)++]) != '\n')
+                break;
+            else
+	        write_line_v2(NULL, options, c, 0);                
+		//printf("%c", *c);
+        }
+    }
+
+}
+
+void parse_line_v2(struct Options* options, char* c, int* b, long* a, char* lbuf,const char* fbuf, int bufsize){
+        if (*c != '\n') {
+	    lbuf[*b]=*c;
+	    char* j = strchr(fbuf + (*a)*sizeof(char), '\n');
+	    if (j == 0){
+		j = strchr(fbuf + (*a)*sizeof(char), '\0');
+	    }
+		//printf("j: %d\n", j);
+	    *b = j - (fbuf + (*a)*sizeof(char)) + 1;
+		//printf("b: %d\n", *b);
+	    strncpy(lbuf + sizeof(char), fbuf + (*a)*sizeof(char), *b);
+	    *a += *b;
+	    //lbuf[(*b)++] = '\0';
+	    //printf("%s", lbuf);
+	    //while ((*c=fbuf[(*a)++]) != '\n' && *a < bufsize)
+            //    lbuf[(*b)++] = *c;
+        }
+
+}
+
+void read_file(FILE* fd, char* fbuf, int bufsize){
+    fseek(fd, 0L, SEEK_SET);
+    fread(fbuf, sizeof(char), bufsize, fd);
+    fbuf[bufsize - 1] = '\0';
+
+}
+
+void nl_v2(FILE* fd, struct Options* options) {
     char c;
     int i = 0;
-    size_t b = 0;
-    unsigned long a = 0;
+    int b = 0;
+    long a = 0;
 
     fseek(fd, 0L, SEEK_END);
     unsigned long bufsize = ftell(fd);
     char* fbuf = malloc(sizeof(char) * bufsize);
     char* lbuf = malloc(sizeof(char) * BUFF_SIZE);
-
-    fseek(fd, 0L, SEEK_SET);
-    fread(fbuf, sizeof(char), bufsize, fd);
-    fbuf[bufsize - 1] = '\0';
+    read_file(fd, fbuf, bufsize);
 
     while ((c = fbuf[a++]) != '\0') {
-        if (c == '\n' && options->blank) {
-            while (c == '\n') {
-                printf("%c", c);
-                c = fbuf[a++];
-            }
-        } else if (c == '\n' && options->join > 0) {
-            while (++i < options->join) {
-                if ((c = fbuf[a++]) != '\n')
-                    break;
-                else
-                    printf("%c", c);
-            }
-        }
-
-        if (c != '\n') {
-            lbuf[b++]=c;
-            while ((c=fbuf[a++]) != '\n' && a < bufsize)
-                lbuf[b++] = c;
-        }
-
+        skip_blanks_v2(options, &i, &c, fbuf, &a);
+	parse_line_v2(options, &c, &b, &a, lbuf, fbuf, bufsize);
         lbuf[b++]='\0';
+	write_line_v2(lbuf, options, &c, 1);
 
-        printf("%d%s%s\n", options->starting, options->separator, lbuf);
 
         b=0;
-        free(lbuf);
-        lbuf = malloc(sizeof(char) * BUFF_SIZE);
+        //free(lbuf);
+        //lbuf = malloc(sizeof(char) * BUFF_SIZE);
 
-        if (!options->increment)
-            options->starting += 1;
-        else
-            options->starting += options->increment;
+        increment(options);
     }
 
     free(fbuf);
     free(lbuf);
 }
 
-int main(int argc, char **argv)
+void parse_args(struct Options* options, int argc, char **argv)
 {
-    int i;
-    FILE* fd;
-    char** f = NULL;
     int option_index = 0;
-
     const struct option long_options[] =
     {
         {"help", no_argument, 0, 'h'},
@@ -133,14 +192,12 @@ int main(int argc, char **argv)
         {"non-blank", no_argument, 0, 't'},
         {0, 0, 0, 0}
     };
-
-    struct options options;
-
-    options.separator = " ";
-    options.starting = 1;
-    options.increment = 0;
-    options.join = 0;
-    options.blank = false;
+	
+    options->separator = " ";
+    options->starting = 1;
+    options->increment = 0;
+    options->join = 0;
+    options->blank = false;
 
     while (1) {
         int c = getopt_long (argc, argv, "hs:v:i:l:t", long_options, &option_index);
@@ -153,40 +210,55 @@ int main(int argc, char **argv)
                 show_help();
                 exit(EXIT_SUCCESS);
             case 's':
-                options.separator = optarg;
+                options->separator = optarg;
                 break;
             case 'v':
-                options.starting = atoi(optarg);
+                options->starting = atoi(optarg);
                 break;
             case 'i':
-                options.increment = atoi(optarg);
+                options->increment = atoi(optarg);
                 break;
             case 'l':
-                options.join = atoi(optarg);
+                options->join = atoi(optarg);
                 break;
             case 't':
-                options.blank = true;
+                options->blank = true;
                 break;
             default:
                 break;
         }
     }
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    FILE* fd;
+    char** f = NULL;
+
+
+    struct Options options;
+    parse_args(&options, argc, argv);	
+
 
     if (optind < argc) {
+
         f = argv + optind;
         for (i = 0; i < argc - optind; ++i) {
             fd = fopen(f[i], "r");
             if (fd != NULL) {
-                nl_v1(fd, &options);
+                //nl_v1(fd, &options);
+		nl_v2(fd, &options);
                 fclose(fd);
             } else {
                 fprintf(stderr, "Error opening file %s\n", f[i]);
             }
         }
     } else {
-        nl_v1(stdin, &options);
-        //nl_v2(stdin, &options);
+        //nl_v1(stdin, &options);
+        nl_v2(stdin, &options);
     }
 
     return 0;
 }
+
