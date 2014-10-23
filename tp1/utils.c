0a33443b0b2db2fd5f3b364fe609728502ff93f1
aschicht <nerhomarek@gmail.com>
2014-10-23 00:47:55 -0300

    ultimos cambios

--- tp1/utils.c
+++ tp1/utils.c
@@ -41,27 +41,18 @@ void print(char* s, int n){
 }
 
 int is_begin_tag(char* newtag, int newtag_l){
-	//print(newtag, newtag_l);
 	if(newtag[0] != '/'){
-		printf("Es un tag de inicio\n");
 		return 0;
 	}
 	return 1;
 }
 
 int compare_tags(char* oldtag, int oldtag_length, char* newtag, int newtag_length){
-	printf("comparando ");
-	print(oldtag, oldtag_length);
-	printf(" con ");
-	print(newtag, newtag_length);
-	printf("\n");
 	if(newtag_length != oldtag_length+1){
-		printf("no son iguales \n");
 		return 0;
 	}else{
 		
 		int i = !memcmp(oldtag, (newtag + 1), oldtag_length);
-		printf("comparo: %d", i);
 		return i;
 	}
 }
@@ -72,44 +63,4 @@ int foo(char* file, int* line, char* oldtag, int* oldtag_length){
 	printf("line: %d\n", *line);
 	(*line)++;
 	return 0;	
-}
-
-int find_tag1(char* file, int* line, char* newtag, int* newtag_length){
-	printf("entro a find tag %d \n", *file);
-	while(*file != '<' && *file != '\0'){
-		if(*file == '\n'){
-
-			(*line)++;
-			printf("se aumento la linea: %d\n", *line);
-		}
-		(file)++;
-		
-		printf("estoy  primer loop %d\n", file);
-	}
-
-	if (*file == '\0'){
-		printf("encontro fin de string\n");
-		return 1;
-	}
-
-	newtag = ++(file);
-	printf("%d\n", file);
-	while(*file != '>' && *file != '\0'){
-		(*newtag_length)++;
-		(file)++;
-
-		printf("newtag encontrado en %d ", file);
-	}
-	if(*newtag_length > 0){
-		printf("new tag (%d)= ", *newtag_length);
-		print(newtag, *newtag_length);
-		printf("\n");
-	}
-
-	if(*file == '\0'){
-		printf("encontro fin de string 2\n");
-		return 1;
-	}
-	printf("salgo de find tag\n");
-	return 0;
 }
